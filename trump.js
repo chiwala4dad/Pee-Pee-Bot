9724da20b284df9173cb3194b063424a29c79c2c
chiwala4dad <chiwala4dad@gmail.com>
2020-07-17 18:54:29 -0400

    Update trump.js

--- trump.js
+++ trump.js
@@ -1,36 +1,14 @@
+const Discord = require('discord.js');
+const client =  new Discord.Client();
 
-const fs = require("fs");
-const _ = require("underscore");
-const moment = require("moment");
-const discord = require('discord.js');
-const client = new discord.Client();
-// store the filenames of all the audio files for sampling later
-const audioFiles = fs.readdirSync("./data");
+client.on("message", (message) => {
 
-client.on("ready", () => {
-   console.log("I'm ready!");
-}); 
+  if(message.content === "cheemz"){
+    then(connection => {
+      connection.play('./data/ITS LIT.mp3');
+   });
+  }
 
 
-client.on("message", async message => {
-	if (message.content === "cheemz")
-	  var voiceChannel = message.member.voiceChannel;
-	  voiceChannel.join().then(connection =>
-{
-     const dispatcher = connection.play('./data/ITS LIT.mp3');
-     dispatcher.on("end", end => {
-       voiceChannel.leave();
-       });
-	  }
-
-    )	   
-   // if the sent message was "ping" then respond with "pong"
-   if (message.content === "Thank you") {
-       message.channel.send("Kanye, Very cool!");
-
-   // if the sent message was "!trump" then play a random trump quote from our data directory
-   } 
-  
-    
 });
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
